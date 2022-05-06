8677588df1948844491118311486a6f4c5067930
Akkia <AkkiaS7@Outlook.com>
2022-05-06 11:46:47 +0800

    init: GET请求处理函数统一添加Get前缀

--- controller/favorite.go
+++ controller/favorite.go
@@ -23,11 +23,11 @@ type FavoriteListResponse struct {
 	VideoList []Video `json:"video_list,omitempty"`
 }
 
-// FavoriteList 获取点赞列表
+// GetFavoriteList 获取点赞列表
 // 登录用户的所有点赞视频列表
 // GET /douyin/favorite/list/
 // https://www.apifox.cn/apidoc/shared-8cc50618-0da6-4d5e-a398-76f3b8f766c5/api-18902464
-func FavoriteList(c *gin.Context) {
+func GetFavoriteList(c *gin.Context) {
 	//userID := c.Query("user_id")         // 用户ID
 	//token := c.Query("token")            // 用户token
 	// TODO 点赞列表接口
